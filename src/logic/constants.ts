7148146698998070b28de2f6eb66ff51f632e7bd
twio142 <64556708+twio142@users.noreply.github.com>
2024-08-11 22:47:18 +0200

    support custom start date

--- src/logic/constants.ts
+++ src/logic/constants.ts
@@ -1,6 +1,6 @@
 export const WORD_LENGTH = 4
 export const TRIES_LIMIT = 10
-export const START_DATE = new Date(2022, 0, 0)
+export const START_DATE = new Date(import.meta.env.VITE_START_DATE || 2022, 0, 0)
 export const RANDOM_SEED = 'handle'
 export const DAYS_PLAY_BACK = 3
 export const DEPLOY_HOST = 'handle.antfu.me'
